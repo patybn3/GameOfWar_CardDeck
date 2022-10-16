1c232d72419677d31a9fc46ecb5eee3650600ae0
patybn3 <paty.nigro@gmail.com>
2022-10-16 19:07:39 -0400

    suits and faces arrays created

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,10 @@
-public class Card {
+import java.util.Stack;
+
+public class Card<T> extends Stack<Card>
+{
+    public static final String[] SUITS = {"CLUBS", "DIAMONDS", "HEARTS", "SPADES"};
+    public static final String[] FACES = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "JACK", "QUEEN", "KING", "ACE"};
+
+    private int suit;
+    private int face;
 }
