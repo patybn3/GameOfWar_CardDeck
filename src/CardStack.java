0acd481eb2533e25e1e7dd954929570afcec841c
patybn3 <paty.nigro@gmail.com>
2022-10-16 19:12:01 -0400

    CardStack.java - constructor, created a loop to insert cards into stack, created a new method = isFull(), finalized push();

--- src/CardStack.java
+++ src/CardStack.java
@@ -1,9 +1,36 @@
 import java.util.Stack;
 
-public class CardStack implements Project1StackInterface<Card>{
+public class CardStack implements Project1StackInterface<Card>
+{
+    private Stack<Card> cardsStack;
+
+    public CardStack()
+    {
+        this.cardsStack = new Stack<>();
+        insertAllCard();;
+    }
+
+    public void insertAllCard()
+    {
+        if(isFull())
+        {
+            throw new StackOverflowException("This Deck is Full.");
+        }
+        else {
+            for (int i = 0; i < 13; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    push(j, i);
+                }
+            }
+        }
+    }
+
     @Override
-    public void push(int suit, int face) {
-        
+    public void push(int suit, int face)
+    {
+        cardsStack.add(new Card(suit, face));
     }
 
     @Override
@@ -34,5 +61,11 @@ public class CardStack implements Project1StackInterface<Card>{
     @Override
     public boolean isEmpty() {
         return false;
+    }
+
+    public boolean isFull()
+    // Returns false - a linked stack is never full
+    {
+        return !cardsStack.isEmpty();
     }
 }
